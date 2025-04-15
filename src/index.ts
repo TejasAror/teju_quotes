ab835f56d62b638c1b0358721c4175cdb0160e71
Tejas <tejasa755@gmail.com>
2025-04-16 05:19:22 +0530

    Get Quote Package

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import quotes from './quotes.js';
+
+
+export const getQuote = () => {
+    const randNum = Math.floor(Math.random() * quotes.length);
+    const getQuote = quotes[randNum];
+    return getQuote;
+};
+
